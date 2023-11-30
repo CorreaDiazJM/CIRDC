0d781b3f156bb6ca4e0ac4b611395ae40b1c12ec
DarthNeo03 <garciacastellanosjs@uvm.edu.ve>
2023-11-30 07:43:09 -0400

    Vistas

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const usuariosRoutes = require('./usuarios.router');
 const documentosRoutes = require('./documentos.router');
 const ventasRoutes = require('./ventas.router');
 const comprasRoutes = require('./compras.router');
+const comentariosRoutes = require('./comentarios.router');
 
 const router = express.Router();
 
@@ -11,6 +12,7 @@ router.use('/usuarios', usuariosRoutes);
 router.use('/documentos', documentosRoutes);
 router.use('/ventas', ventasRoutes);
 router.use('/compras', comprasRoutes);
+router.use('/comentarios', comentariosRoutes);
 
 
 module.exports = router;
